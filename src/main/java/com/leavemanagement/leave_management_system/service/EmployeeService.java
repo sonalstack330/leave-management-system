76ace13723391c1f02f53de32aa87362fbecad1e
Sonal Gajbhiye <sonalgajbhiye2001@gmail.com>
2026-07-29 21:25:13 +0530

    Add EmployeeService with basic employee CRUD operations

---
+++ src/main/java/com/leavemanagement/leave_management_system/service/EmployeeService.java
@@ -0,0 +1,33 @@
+package com.leavemanagement.leave_management_system.service;
+
+import com.leavemanagement.leave_management_system.entity.Employee;
+import com.leavemanagement.leave_management_system.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Employee not found with id: " + id));
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getEmployeesByManager(Long managerId) {
+        return employeeRepository.findByManagerId(managerId);
+    }
+}
---
+++ src/main/java/com/leavemanagement/leave_management_system/service/LeaveRequestService.java
@@ -0,0 +1,4 @@
+package com.leavemanagement.leave_management_system.service;
+
+public class LeaveRequestService {
+}
