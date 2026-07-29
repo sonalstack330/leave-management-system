b6f60422893f869c94dd7519152415a0a12f94a6
Sonal Gajbhiye <sonalgajbhiye2001@gmail.com>
2026-07-29 21:21:46 +0530

    Add LeaveReviewDTO for manager approval/rejection request body

--- src/main/java/com/leavemanagement/leave_management_system/dto/LeaveReviewDTO.java
+++ src/main/java/com/leavemanagement/leave_management_system/dto/LeaveReviewDTO.java
@@ -1,4 +1,14 @@
 package com.leavemanagement.leave_management_system.dto;
 
+import com.leavemanagement.leave_management_system.entity.LeaveStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class LeaveReviewDTO {
+
+    @NotNull
+    private LeaveStatus status; // APPROVED or REJECTED
+
+    private String reviewComment;
 }
