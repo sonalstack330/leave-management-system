0ba942d7aa6e879a59e863a797d7e107122d1e75
Sonal Gajbhiye <sonalgajbhiye2001@gmail.com>
2026-07-29 20:41:55 +0530

    Initial commit - project setup

---
+++ src/main/java/com/leavemanagement/leave_management_system/LeaveManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.leavemanagement.leave_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveManagementSystemApplication.class, args);
+	}
+
+}
