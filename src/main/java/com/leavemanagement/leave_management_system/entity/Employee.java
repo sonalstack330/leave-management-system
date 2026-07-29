a026d5ce7d209213234742cddd81b3987e1e88b4
Sonal Gajbhiye <sonalgajbhiye2001@gmail.com>
2026-07-30 05:23:05 +0530

    Fix Hibernate proxy fields leaking into JSON response by ignoring hibernateLazyInitializer and handler

--- src/main/java/com/leavemanagement/leave_management_system/entity/Employee.java
+++ src/main/java/com/leavemanagement/leave_management_system/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.leavemanagement.leave_management_system.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Employee {
 
     @Id
