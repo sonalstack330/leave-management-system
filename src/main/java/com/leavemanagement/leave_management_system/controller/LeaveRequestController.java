327be9e385caf5b5117f09e2e45afc251abf4283
Sonal Gajbhiye <sonalgajbhiye2001@gmail.com>
2026-08-02 22:17:20 +0530

    Remove redundant @CrossOrigin annotation now handled by global CorsConfig

--- src/main/java/com/leavemanagement/leave_management_system/controller/LeaveRequestController.java
+++ src/main/java/com/leavemanagement/leave_management_system/controller/LeaveRequestController.java
@@ -15,7 +15,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/leave-requests")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
 public class LeaveRequestController {
 
     private final LeaveRequestService leaveRequestService;
